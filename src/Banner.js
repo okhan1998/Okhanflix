a53d124891af347d8da7091426ec3e7c3cbad108
okhan1998 <whwlgns111@gmail.com>
2021-11-12 01:32:48 +0900

    Deploy webapp

--- src/Banner.js
+++ src/Banner.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import { useEffect } from 'react/cjs/react.development';
+import React, {useState} from 'react';
+import { useEffect } from 'react';
 import axios from './axios';
 import requests from './requests'
 import './Banner.css';
--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import { useEffect } from 'react/cjs/react.development';
+import React, {useState} from 'react';
+import { useEffect } from 'react';
 import './Nav.css';
 function Nav() {
 
